2cc835d2b9de4b4022bed83acd32a750923ef5e1
Jose M. Vasconcellos <miguel_vasconce@hotmail.com>
2019-01-16 11:06:50 -0500

    Some cosmetic changes

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -17,16 +17,16 @@ class Book extends Component {
                   <img src={ this.props.articleThumbnail } style={{width: "100%", height: "100%"}} alt="something"/>
               </Col>
               <Col xs={12} sm={2} md={2}>
-                  <label> { this.props.title } </label>
+                  { this.props.title }
               </Col>
               <Col xs={12} sm={2} md={2}>
-                  <label> { this.props.summary } </label>
+                  { this.props.summary }
               </Col>
               <Col xs={12} sm={2} md={2}>
-                  <label> { this.props.author } </label>
+                  { this.props.author }
               </Col>
               <Col xs={12} sm={2} md={2}>
-              <label> { this.props.PublishedDate } </label>
+              { this.props.PublishedDate }
               </Col>
               <Col xs={12} sm={2} md={2}>
               <a href={ this.props.link }>see more...</a>
@@ -37,22 +37,22 @@ class Book extends Component {
               <Col xs={12} sm={6} md={6}>
                   <Row>
                     <Col xs={12} sm={12} md={12}>
-                      <label> { "Title: " + this.props.title } </label>
+                      <label> { "Title: "} </label> {this.props.title}
                     </Col>
                   </Row>
                   <Row>
                     <Col xs={12} sm={12} md={12}>
-                      <label> { "Summary: " + this.props.summary } </label>
+                      <label> { "Summary: "} </label> {this.props.summary}
                     </Col>
                   </Row>
                   <Row>
                     <Col xs={12} sm={12} md={12}>
-                      <label> { "Author: " + this.props.author } </label>
+                      <label> { "Author: "} </label> {this.props.author}
                     </Col>
                   </Row>
                   <Row>
                     <Col xs={12} sm={12} md={12}>
-                      <label> { "Published Date: " + this.props.PublishedDate } </label>
+                      <label> { "Published Date: "} </label> {this.props.PublishedDate}
                     </Col>
                   </Row>
                   <Row>
